95bd3a87e538bd458f790bc04a83c53151c7c405
Jerry Nixon <1749983+JerryNixon@users.noreply.github.com>
2021-11-04 13:39:02 -0600

    Added test to src.

---
+++ StringConcatBenchmark.Tests/UnitTests.cs
@@ -0,0 +1,64 @@
+using NUnit.Framework;
+
+namespace StringConcatBenchmark
+{
+    public class Tests
+    {
+        private TestHarness _tests;
+
+        [SetUp]
+        public void Setup()
+        {
+            _tests = new TestHarness();
+        }
+
+        [Test]
+        public void StringPlus()
+        {
+            var result = _tests.StringPlus();
+            Assert.AreEqual(result, Program.result);
+        }
+
+        [Test]
+        public void StringPlusEquals()
+        {
+            var result = _tests.StringPlusEquals();
+            Assert.AreEqual(result, Program.result);
+        }
+
+        [Test]
+        public void StringInterpolation()
+        {
+            var result = _tests.StringInterpolation();
+            Assert.AreEqual(result, Program.result);
+        }
+
+        [Test]
+        public void StringFormat()
+        {
+            var result = _tests.StringFormat();
+            Assert.AreEqual(result, Program.result);
+        }
+
+        [Test]
+        public void StringConcat()
+        {
+            var result = _tests.StringConcat();
+            Assert.AreEqual(result, Program.result);
+        }
+
+        [Test]
+        public void StringJoin()
+        {
+            var result = _tests.StringJoin();
+            Assert.AreEqual(result, Program.result);
+        }
+
+        [Test]
+        public void StringBuilder()
+        {
+            var result = _tests.StringBuilder();
+            Assert.AreEqual(result, Program.result);
+        }
+    }
+}
